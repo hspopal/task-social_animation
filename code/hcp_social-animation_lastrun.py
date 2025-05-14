e96784eaef8bb42f359a38bf22293b60f60bb3ac
hspopal <hpopal@bsosmwpsybps031.umd.edu>
2025-05-14 10:17:20 -0400

    last day

--- code/hcp_social-animation_lastrun.py
+++ code/hcp_social-animation_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.2),
-    on Mon Feb 24 16:21:12 2025
+    on Mon Feb 24 16:30:01 2025
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
